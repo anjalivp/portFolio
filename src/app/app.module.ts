1b118e34060bf3afca163f5bd7c96932ae2e3c5c
anjali <anjalivp92@gmail.com>
2022-07-19 20:11:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { SamplesComponent } from './samples/samples.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    SamplesComponent,
+    ContactComponent,
+    LandingPageComponent,
+    ServicesComponent,
+    TestimonyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
